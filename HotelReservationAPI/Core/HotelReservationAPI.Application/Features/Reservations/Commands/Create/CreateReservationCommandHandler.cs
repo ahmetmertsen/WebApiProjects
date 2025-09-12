52e08a71c81f7c7fa1af65b69ffc4a28d6b11cb7
Ahmet <ahmetmert0505@gmail.com>
2025-09-12 21:57:29 +0300

    Project2(HRApi): CQRS Pattern için gerekli dosyalar ve classlar oluşturuldu.

---
+++ HotelReservationAPI/Core/HotelReservationAPI.Application/Features/Reservations/Commands/Create/CreateReservationCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationAPI.Application.Features.Reservations.Commands.Create
+{
+    public class CreateReservationCommandHandler
+    {
+    }
+}
---
+++ HotelReservationAPI/Core/HotelReservationAPI.Application/Features/Reservations/Queries/GetById/GetByIdReservationRequestHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationAPI.Application.Features.Reservations.Queries.GetById
+{
+    public class GetByIdReservationRequestHandler
+    {
+    }
+}
