d881940c5d09777281250542a947c1bb9e8f0fe7
Ahmet <ahmetmert0505@gmail.com>
2025-09-15 19:08:16 +0300

    Project2(HRApi): ReservationDto kaynaklı olan rezervasyon listelemede customerId ve roomId'lerin null dönme sorunu çözüldü.

--- HotelReservationAPI/Core/HotelReservationAPI.Application/Dtos/ReservationDto.cs
+++ HotelReservationAPI/Core/HotelReservationAPI.Application/Dtos/ReservationDto.cs
@@ -10,8 +10,8 @@ namespace HotelReservationAPI.Application.Dtos
     public class ReservationDto
     {
         public int Id { get; set; }
-        public Customer Customer { get; set; }
-        public Room Room { get; set; }
+        public int CustomerId { get; set; }
+        public int RoomId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
     }
